1f0dc52b01cd4a2c3dfbe7eb6558b281d700035a
Eraldo Ribeiro <eribeiro@cs.fit.edu>
2025-01-22 09:48:47 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh -l
 
-cd /root/os161/root/
-/root/os161/tools/bin/sys161 kernel q > output.txt 
-cat output.txt
-
-the_string=$(grep "DUMBVM" "output.txt")
-echo $the_string | grep -oP '.*(?=system version)'
-var=$(echo $the_string | grep -oP '.*(?=system version)')
-
 function assert {
   if [ "$1" ]; then
     echo "Assertion passed"
@@ -16,6 +8,14 @@ function assert {
     exit 1
   fi
 }
+
+cd /root/os161/root/
+/root/os161/tools/bin/sys161 kernel q > output.txt 
+cat output.txt
+
+the_string=$(grep "DUMBVM" "output.txt")
+echo $the_string | grep -oP '.*(?=system version)'
+var=$(echo $the_string | grep -oP '.*(?=system version)')
 
 assert "$var" "Put-your-group-name-here's"
 
