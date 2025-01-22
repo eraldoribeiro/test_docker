7008da4972812bc4b73d4ed0d9a6b0c7b6118816
Eraldo Ribeiro <eribeiro@cs.fit.edu>
2025-01-22 08:21:34 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@ cd /root/os161/root/
 /root/os161/tools/bin/sys161 kernel q > output.txt 
 message="$(cat output.txt)"
 EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
-echo "text<<$EOF" >> $GITHUB_OUTPUT
-echo "$message" >> $GITHUB_OUTPUT
-echo "$EOF" >> $GITHUB_OUTPUT
+message=$(echo $message | tr '\n' ' ')
+echo "text=$message" >> $GITHUB_OUTPUT
 # cat output.txt >> $GITHUB_OUTPUT
 
