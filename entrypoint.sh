9847f82d3f8b18d5c7f0bfe6c3e823b3c838b214
Eraldo Ribeiro <eribeiro@cs.fit.edu>
2025-01-22 09:45:29 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,18 @@ cat output.txt
 
 the_string=$(grep "DUMBVM" "output.txt")
 echo $the_string | grep -oP '.*(?=system version)'
+var=$(echo $the_string | grep -oP '.*(?=system version)')
 
+function assert {
+  if [ "$1" ]; then
+    echo "Assertion passed"
+  else
+    echo "Assertion failed: $2"
+    exit 1
+  fi
+}
 
+assert "$var" "Put-your-group-name-here's"
 
 
 
