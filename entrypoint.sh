e3d6290528dd108d357b1e55a72118598b068f80
Eraldo Ribeiro <eribeiro@cs.fit.edu>
2025-01-22 07:25:36 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-ls -l 
-sys161 kernel q
-time=$(date)
-echo "This is the time: $time"
-echo "time=$time" >> $GITHUB_OUTPUT
+cd /root/os161/root/
+/root/os161/tools/bin/sys161 kernel q >> $GITHUB_OUTPUT
 
