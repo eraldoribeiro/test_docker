7db58d1c5fc5798050c5441a6c7e96c0581fe3c2
Eraldo Ribeiro <eribeiro@cs.fit.edu>
2025-01-22 09:19:02 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,11 @@ cd /root/os161/root/
 /root/os161/tools/bin/sys161 kernel q > output.txt 
 cat output.txt
 
+the_string=$(grep "DUMBVM" "output.txt")
+echo $the_string | grep -oP '.*(?=system version)'
+
+
+
+
+
 
