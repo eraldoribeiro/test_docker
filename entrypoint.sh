c320be7efc230b83dd0637be8b2682d88d2b4655
Eraldo Ribeiro <eribeiro@cs.fit.edu>
2025-01-22 09:54:02 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/sh -l
 
-function assert {
-  if [ "$1" ]; then
-    echo "Assertion passed"
-  else
-    echo "Assertion failed: $2"
-    exit 1
-  fi
-}
-
 cd /root/os161/root/
 /root/os161/tools/bin/sys161 kernel q > output.txt 
 cat output.txt
@@ -17,7 +8,13 @@ the_string=$(grep "DUMBVM" "output.txt")
 echo $the_string | grep -oP '.*(?=system version)'
 var=$(echo $the_string | grep -oP '.*(?=system version)')
 
-assert "$var" "Put-your-group-name-here's"
+if [ "$var" != "Put-your-group-name-here's" ]
+then
+    echo "No changes in line containing Put-your-group-name-here's"
+    exit 1
+fi
+
+
 
 
 
