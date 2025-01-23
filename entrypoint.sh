9703b4bbb031807236354c697c2fcf3f7c2e8c76
Eraldo Ribeiro <eribeiro@cs.fit.edu>
2025-01-23 00:30:18 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/sh -l
 
+# Build OS/161 (Kernel-level)
+cd kern/conf
+./config DUMBVM
+cd ../compile/DUMBVM
+bmake depend
+bmake
+bmake install
+
+# Build OS/161 (Userland)
+cd ~
+bmake includes
+bmake
+bmake install
+
+# Run the OS/161 kernel and save the output to a file
 cd /root/os161/root/
 /root/os161/tools/bin/sys161 kernel q > output.txt 
 cat output.txt
